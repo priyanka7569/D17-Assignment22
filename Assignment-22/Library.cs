87c316494f92d6bec7dbb012b5f7a75906f1c5e1
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-28 16:39:41 +0530

    assignment modified

---
+++ Assignment-22/Library.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+    internal class Librarycs : Book
+    {
+        List<Book> librarycs = new List<Book>();
+        public void AddBook(int id, string name, string author, string genre, bool available)
+        {
+            Book newBook = new Book()
+            {
+                BookId = id,
+                Title = name,
+                Author = author,
+                Genre = genre,
+                IsAvailable = available,
+            };
+            librarycs.Add(newBook);
+
+        }
+        public void PrintAllBooks()
+        {
+            foreach (var book in librarycs)
+            {
+                Console.WriteLine("Book Id: " + book.BookId);
+                Console.WriteLine("Title of the Book: " + book.Title);
+                Console.WriteLine("Author of the Book: " + book.Author);
+                Console.WriteLine("Genre of the Book: " + book.Genre);
+                Console.WriteLine("Is Book available for borrowing: " + book.IsAvailable);
+            }
+        }
+
+        public void SearchById(int id)
+        {
+            Book matchingbook = librarycs.Find(book => book.BookId == id);
+            if (matchingbook != null)
+            {
+
+                Console.WriteLine("Book Id: " + matchingbook.BookId);
+                Console.WriteLine("Title of the Book: " + matchingbook.Title);
+                Console.WriteLine("Author of the Book: " + matchingbook.Author);
+                Console.WriteLine("Genre of the Book: " + matchingbook.Genre);
+                Console.WriteLine("Is Book available for borrowing: " + matchingbook.IsAvailable);
+            }
+            else
+            {
+                Console.WriteLine("Id does not exist");
+            }
+        }
+
+        public void SearchByTitle(string name)
+        {
+            Book matchingbook = librarycs.Find(book => book.Title == name);
+            if (matchingbook != null)
+            {
+                Console.WriteLine("Book Id: " + matchingbook.BookId);
+                Console.WriteLine("Title of the Book: " + matchingbook.Title);
+                Console.WriteLine("Author of the Book: " + matchingbook.Author);
+                Console.WriteLine("Genre of the Book: " + matchingbook.Genre);
+                Console.WriteLine("Is Book available for borrowing: " + matchingbook.IsAvailable);
+            }
+            else
+            {
+                Console.WriteLine("Title does not exist");
+            }
+        }
+
+
+    }
+}
---
+++ Assignment-22/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            Librarycs librarycs = new Librarycs();
+            do
+            {
+
+                Console.WriteLine("Choose the operation 1.Add Books 2.View All Books 3.SearchById 4.SearchByName 5.Exit the Program");
+                int op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter Book id");
+                            int id = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Book Name");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter Author name");
+                            string author = Console.ReadLine();
+                            Console.WriteLine("Enter Genre");
+                            string genre = Console.ReadLine();
+                            Console.WriteLine("Is Book Available to Borrow?");
+                            bool avail = bool.Parse(Console.ReadLine());
+                            librarycs.AddBook(id, name, author, genre, avail);
+                            break;
+                        }
+                    case 2:
+                        {
+                            librarycs.PrintAllBooks();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter Id to be searched");
+                            int id = int.Parse(Console.ReadLine());
+                            librarycs.SearchById(id);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter name to be searched");
+                            string name = Console.ReadLine();
+                            librarycs.SearchByTitle(name);
+                            break;
+                        }
+                    case 5:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Operation Choice");
+                            break;
+                        }
+                }
+                Console.WriteLine("Do You wanna Continue? press y/n");
+                choice = Console.ReadLine();
+            } while (choice == "y");
+
+        }
+    }
+}
+        
+    
+
